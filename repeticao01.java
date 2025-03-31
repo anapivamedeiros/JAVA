007445fa6d986a87002d5bae5fdeb7ef8e79b3ad
anapivamedeiros <ana.piva1a@gmail.com>
2025-03-31 11:48:27 -0300

    Contagem de numero

---
+++ repeticao01.java
@@ -0,0 +1,18 @@
+/*
+ exemplo de repetição
+*/
+import java.util.Scanner;
+public class repeticao01
+{
+	public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
+	int n;
+	
+	System.out.print("Digite um numero maior que zero: ");
+	n= ler.nextInt ();
+	
+	for(int i = 0; i <= n; i++) {
+	    System.out.println("numero "+ i);
+	}
+	}
+}
