2d6700ad42f89d58181c05d17502318aa65f7f05
anapivamedeiros <ana.piva1a@gmail.com>
2025-04-07 11:16:51 -0300

    Mostre os numeros de 0 ate o limite definido

---
+++ Ex26.java
@@ -0,0 +1,19 @@
+/*
+Ex26- Mostre na tela de 0 até N, em que N é o limite inserido pelo usuário.
+*/
+import java.util.Scanner;
+public class Ex26
+{
+	public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
+	int numero = 0;
+	
+	System.out.print("Digite o numero limite: ");
+	int n = scanner.nextInt();
+	
+	while (numero <= n) {
+	System.out.println(numero);
+	numero++;
+	}
+}
+}
